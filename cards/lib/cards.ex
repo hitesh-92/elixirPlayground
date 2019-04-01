5387aea9f891ea8a6b70d27b08c5a0a03f276b43
hitesh-92 <hiteshgohil@hotmail.co.uk>
2019-04-01 05:16:00 +0100

    added init deck methods

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -16,8 +16,34 @@ defmodule Cards do
     :world
   end
 
-  def init do
-    "hello world!"
+  def create_deck do
+    #Array
+    values = ["Ace", "King", "Quenn", "Jack", "Two"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # for suit <- suits do
+    #   suit
+    # end
+
+    # cards = for value <- values do
+    #   for suit <- suits do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
+    # List.flatten(cards)
+
+    #no need to flatten list
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck,card)
   end
 
 end
