20465c45b884d23da1e90f81abe49929a6be5741
hitesh-92 <hiteshgohil@hotmail.co.uk>
2019-04-01 02:11:10 +0100

    init

---
+++ cards/lib/cards.ex
@@ -0,0 +1,23 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def init do
+    "hello world!"
+  end
+
+end
